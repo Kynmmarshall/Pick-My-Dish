62cbd6e6efd450c03b91d0eb6c8eef4ff0a330c4
Kynmmarshall <kynmmarshall@gmail.com>
2025-11-05 07:50:59 +0100

    started home Screen

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,14 +1,57 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("HOME PAGE"),
-      ),
-    );
+    return  Scaffold(
+      body:Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Stack(
+          children: [
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: BoxDecoration(
+              gradient: LinearGradient(
+              colors: [
+                Colors.black,
+                const Color.fromARGB(255, 97, 96, 96),
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+
+                )
+              )
+            ),
+
+          Padding(padding: EdgeInsets.all(30),
+              child: Center(
+                child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                  SizedBox(height: 20,),
+
+                  Image.asset(
+                  'assets/icons/hamburger.png',
+                  width: 24,
+                  height: 24,
+                )
+                  ]
+                ),
+               ),
+              ),
+          )
+          ],
+        ),
+      )
+      );
   }
 }
--- lib/Screens/splash_screen.dart
+++ lib/Screens/splash_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:pick_my_dish/Screens/Register_screen.dart';
+import 'package:pick_my_dish/Screens/home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -18,7 +19,7 @@ class _SplashScreenState extends State<SplashScreen> {
     _timer = Timer(const Duration(seconds: 3),() {
       Navigator.push(
       context, 
-      MaterialPageRoute(builder:(context) => const RegisterScreen()),
+      MaterialPageRoute(builder:(context) => const HomeScreen()),
       );
     });
     
