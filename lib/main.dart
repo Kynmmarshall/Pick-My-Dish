61c0004b9bb51f96a522624e963aee27400ba366
Fahdil <nadaljunior999@gmail.com>
2025-12-06 23:04:37 +0100

    Add multiple screens and update main app structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:pick_my_dish/Providers/recipe_provider.dart';
 import 'package:pick_my_dish/Providers/user_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:pick_my_dish/Screens/splash_screen.dart';
+import 'package:pick_my_dish/Screens/login_screen.dart'; // ADDED
+import 'package:pick_my_dish/Screens/register_screen.dart'; // ADDED
+import 'package:pick_my_dish/Screens/home_screen.dart'; // ADDED
+import 'package:pick_my_dish/Screens/ingredient_input_screen.dart'; // ADDED
+import 'package:pick_my_dish/Screens/recipe_screen.dart'; // ADDED
+import 'package:pick_my_dish/Screens/favorite_screen.dart'; // ADDED
+import 'package:pick_my_dish/Screens/profile_screen.dart'; // ADDED
+import 'package:pick_my_dish/Screens/recipe_upload_screen.dart'; // ADDED
 
 void main() {
-   runApp(
+  runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => UserProvider()),
+        ChangeNotifierProvider(create: (_) => RecipeProvider()),
       ],
       child: const PickMyDish(),
     ),
@@ -19,9 +29,20 @@ class PickMyDish extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: const SplashScreen(),
+      routes: {
+        '/': (context) => const SplashScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/register': (context) => const RegisterScreen(),
+        '/home': (context) => const HomeScreen(),
+        '/ingredients': (context) => const IngredientInputScreen(),
+        '/recipes': (context) => const RecipesScreen(),
+        '/favorites': (context) => const FavoritesScreen(),
+        '/profile': (context) => const ProfileScreen(),
+        '/upload': (context) => const RecipeUploadScreen(),
+      },
     );
   }
 }
